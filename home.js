c49d81cb053df3ef251b8c248c2455638e9de2d8
Krishna Paanchajanya <60803066+PAANCHAJANYA@users.noreply.github.com>
2024-06-08 19:30:38 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,55 @@
+function contactUsClicked()
+{
+    for(var i of document.querySelectorAll("body > section"))
+    {
+        i.style.display = "none";
+    }
+    for(var i of document.querySelectorAll(".contact-us-section"))
+    {
+        i.style.display = "block";
+    }
+}
+function homeClicked()
+{
+    for(var i of document.querySelectorAll("body > section"))
+    {
+        i.style.display = "none";
+    }
+    for(var i of document.querySelectorAll(".home-section"))
+    {
+        i.style.display = "block";
+    }
+}
+function examsClicked()
+{
+    for(var i of document.querySelectorAll("body > section"))
+    {
+        i.style.display = "none";
+    }
+    for(var i of document.querySelectorAll(".exams-section"))
+    {
+        i.style.display = "block";
+    }
+}
+function awardsClicked()
+{
+    for(var i of document.querySelectorAll("body > section"))
+    {
+        i.style.display = "none";
+    }
+    for(var i of document.querySelectorAll(".awards-section"))
+    {
+        i.style.display = "block";
+    }
+}
+function galleryClicked()
+{
+    for(var i of document.querySelectorAll("body > section"))
+    {
+        i.style.display = "none";
+    }
+    for(var i of document.querySelectorAll(".gallery-section"))
+    {
+        i.style.display = "block";
+    }
+}
